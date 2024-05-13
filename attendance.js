b67d335bb2658b2ac85e6bfcf05844b47ca9c8a2
FainNovity <160344269+FainNovity@users.noreply.github.com>
2024-05-13 13:36:35 +0530

    Add files via upload

--- attendance.js
+++ attendance.js
@@ -4,8 +4,10 @@ var dt= new Date();
 var year= dt.getFullYear();
 var month= dt.getMonth()+1;
 var date= dt.getDate();
+var len=0;
 
 //window.open(`https://wa.me/9662490058?text=I%27m%20api%20msg%20hello%20Harsh%20friend%20Hello`, '_blank');
+
 function playIt(){
     var ele=document.getElementById('show');
     ele.style.animationPlayState="running";
@@ -73,4 +75,77 @@ function absent(){
     currentRoll+=1;
     update();
     playIt();
+}
+    function start() {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  var commands = {
+    'yes :variable': function(variable){
+    console.log(variable);
+      if(variable=="present"){
+          present();
+      }
+      else if(variable=="absent"){
+          absent();
+      }
+    },
+    'no :variable': function(variable){
+    console.log(variable);
+      if(variable=="present"){
+          present();
+      }
+      else if(variable=="absent"){
+          absent();
+      }
+    },
+    'class :variable': function(variable){
+    console.log(variable);
+      if(variable.toLowerCase()=="a"){
+         document.getElementById('a').checked= true;
+ 
+      }
+      else if(variable.toLowerCase()=="b"){
+         document.getElementById('b').checked= true;
+      }
+    },
+    'next :variable': function(variable){
+    console.log(variable);
+      if(variable= "class"){
+         changeClass();
+ 
+      }
+      
+    },
+    'send :variable': function(variable){
+    console.log(variable);
+      if(variable= "list"){
+         send(); 
+      }
+      
+    },
+    'remove :variable': function(variable){
+    console.log(variable);
+      if(parseInt(variable)<200 && parseInt(variable)>100){
+      document.getElementById('A').value= document.getElementById('A').value.split(variable+", ").join("");  
+ console.log(document.getElementById('A').value);
+      }
+      else if(parseInt(variable)<300 && parseInt(variable)>200){
+      
+     document.getElementById('B').value= document.getElementById('B').value.split(variable+", ").join("");  
+ 
+      }
+      else if(parseInt(variable)>300){
+      document.getElementById('C').value= document.getElementById('C').value.split(variable+", ").join("");  
+ 
+      }
+    }
+  };
+
+  annyang.addCommands(commands);
+
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+}
+function stop(){
+    annyang.start({ autoRestart: false, continuous: false });
 }
